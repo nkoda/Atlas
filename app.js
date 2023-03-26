044dbb90ff83317c0b34f4fb50dd7cce21512a62
Nikko <dumriquenikko@gmail.com>
2023-03-26 16:48:15 -0700

    added 404 page

--- app.js
+++ app.js
@@ -13,5 +13,9 @@ app.use((req, res, next) => {
 
 app.use('/api', publicRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1> 404: Page not found.</h1>')
+})
+
 app.listen(3000);
 
