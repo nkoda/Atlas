ced434df94b41cf07cfcc11cadb9c39ac8fadc9a
Nikko <dumriquenikko@gmail.com>
2023-03-27 19:33:05 -0700

    cleaned up products.js and implemented getAllProducts()

--- backend/controllers/products.js
+++ backend/controllers/products.js
@@ -3,38 +3,35 @@ const path = require('path');
 
 const Product = require('../models/products');
 
+//GET request to get all products
 exports.getAllProducts = (req, res, next) => {
-    //TODO
-    // Retrieve all the products
-    // Product.fetchAll()
-    res.status(200).json({
-        productId: 2000,
-        productName: "Project TTEST",
-        productOwnerName: "William Perez",
-        Developers: [
-            "Grace Lee",
-            "Michael Garcia",
-            "Victoria Johnson",
-            "Evan Smith",
-            "Audrey Davis"
-        ],
-        scrumMasterName: "Nicholas Kim",
-        startDate: "2022/11/01",
-        methodology: "XP"
+    Product.fetchAll(products => {
+        res.status(200).json(products)
     });
 }
 
 //POST request to create new product on the server
 exports.createProduct = (req, res, next) => {
-    const id = req.params.id
-    const name = req.query.productName
-    const ownerName = req.query.productOwnerName;
-    const developers = req.query.developers;
-    const scrumMasterName = req.query.scrumMasterName;
-    const startDate = req.query.startDate;
-    const methodology = req.query.methodology;
+    const {
+        productId,
+        productName,
+        productOwnerName,
+        developers,
+        scrumMasterName,
+        startDate,
+        methodology
+      } = req.query;
 
-    const product = new Product(id, name, ownerName, developers, scrumMasterName, startDate, methodology);
+      const product = new Product({
+        productId,
+        productName,
+        productOwnerName,
+        developers,
+        scrumMasterName,
+        startDate,
+        methodology
+      });
+
     product.save()
     res.status(201).json(JSON.stringify(product));
 
